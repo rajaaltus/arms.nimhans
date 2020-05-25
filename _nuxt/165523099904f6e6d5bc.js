a482f2aa3068fe66c64eb6400838e56dbeb1bb1f
Raja s <rajaaltus@gmail.com>
2020-05-25 14:03:00 +0700

    genie 2 launched

---
+++ _nuxt/165523099904f6e6d5bc.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{799:function(n,o,l){"use strict";l.r(o);var e=l(42),component=Object(e.a)({},void 0,void 0,!1,null,null,null);o.default=component.exports}}]);
